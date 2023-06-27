19b81cca66ed68ac72be8bc2d39b88e638315a26
jaleelnickson <136690980+jaleelnickson@users.noreply.github.com>
2023-06-27 12:02:20 +0700

    Implement user feedback

---
+++ flower_nature_house.c
@@ -0,0 +1,308 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Paths to Peace\n");
+
+    // 1. Respect human rights
+    printf("1. Respect human rights\n");
+   
+    // 2. Participate in peaceful dialogue
+    printf("2. Participate in peaceful dialogue\n");
+   
+    // 3. Disarmament for peace
+    printf("3. Disarmament for peace\n");
+   
+    // 4. Non-violent action
+    printf("4. Non-violent action\n");
+   
+    // 5. Acceptance of international law
+    printf("5. Acceptance of international law\n");
+   
+    // 6. End impunity for violations of international law
+    printf("6. End impunity for violations of international law\n");
+   
+    // 7. Equity of rights and responsibilities
+    printf("7. Equity of rights and responsibilities\n");
+   
+    // 8. End occupation and colonialism
+    printf("8. End occupation and colonialism\n");
+   
+    // 9. Recognition of interdependence
+    printf("9. Recognition of interdependence\n");
+   
+    // 10. Promote democracy
+    printf("10. Promote democracy\n");
+   
+    // 11. Promote disarmament
+    printf("11. Promote disarmament\n");
+   
+    // 12. Promote resolution of territorial disputes
+    printf("12. Promote resolution of territorial disputes\n");
+   
+    // 13. Foster mutual respect
+    printf("13. Foster mutual respect\n");
+   
+    // 14. Foster mutual non-aggression
+    printf("14. Foster mutual non-aggression\n");
+   
+    // 15. Promote mutual development
+    printf("15. Promote mutual development\n");
+   
+    // 16. Reduce arms production
+    printf("16. Reduce arms production\n");
+   
+    // 17. Respect social justice
+    printf("17. Respect social justice\n");
+   
+    // 18. Respect the rights of minorities
+    printf("18. Respect the rights of minorities\n");
+   
+    // 19. Respect the rule of law
+    printf("19. Respect the rule of law\n");
+   
+    // 20. Respect the sovereignty of states
+    printf("20. Respect the sovereignty of states\n");
+   
+    // 21. Promote understanding
+    printf("21. Promote understanding\n");
+   
+    // 22. Promote education
+    printf("22. Promote education\n");
+   
+    // 23. Promote economic exchange
+    printf("23. Promote economic exchange\n");
+   
+    // 24. Create an international criminal court
+    printf("24. Create an international criminal court\n");
+   
+    // 25. Promote human rights
+    printf("25. Promote human rights\n");
+   
+    // 26. Promote civil society
+    printf("26. Promote civil society\n");
+   
+    // 27. Promote regional confederations
+    printf("27. Promote regional confederations\n");
+   
+    // 28. Foster sustainable development
+    printf("28. Foster sustainable development\n");
+   
+    // 29. Promote population control
+    printf("29. Promote population control\n");
+   
+    // 30. Promote gender equality
+    printf("30. Promote gender equality\n");
+   
+    // 31. Eradicate poverty
+    printf("31. Eradicate poverty\n");
+   
+    // 32. Reduce the risk of nuclear war
+    printf("32. Reduce the risk of nuclear war\n");
+   
+    // 33. Work towards a world order based on justice
+    printf("33. Work towards a world order based on justice\n");
+   
+    // 34. Combat global terrorism
+    printf("34. Combat global terrorism\n");
+   
+    // 35. Ban weapons of mass destruction
+    printf("35. Ban weapons of mass destruction\n");
+   
+    // 36. End the arms trade
+    printf("36. End the arms trade\n");
+   
+    // 37. Support the promotion of democracy
+    printf("37. Support the promotion of democracy\n");
+   
+    // 38. Develop international dispute-resolution mechanisms 
+    printf("38. Develop international dispute-resolution mechanisms \n");
+   
+    // 39. Support international organizations
+    printf("39. Support international organizations\n");
+   
+    // 40. Reduce the threat of conventional weapons
+    printf("40. Reduce the threat of conventional weapons\n");
+   
+    // 41. Provide humanitarian assistance
+    printf("41. Provide humanitarian assistance\n");
+   
+    // 42. Develop conflict-resolution mechanisms
+    printf("42. Develop conflict-resolution mechanisms\n");
+   
+    // 43. Promote economic integration
+    printf("43. Promote economic integration\n");
+   
+    // 44. Establish regional security organizations
+    printf("44. Establish regional security organizations\n");
+   
+    // 45. Promote international environmental protection
+    printf("45. Promote international environmental protection\n");
+   
+    // 46. Work towards arms control agreements
+    printf("46. Work towards arms control agreements\n");
+   
+    // 47. Implement international disarmament initiatives
+    printf("47. Implement international disarmament initiatives\n");
+   
+    // 48. Promote human security
+    printf("48. Promote human security\n");
+   
+    // 49. Educate for peace
+    printf("49. Educate for peace\n");
+   
+    // 50. Promote regional economic cooperation
+    printf("50. Promote regional economic cooperation\n");
+   
+    // 51. Foster cultural understanding
+    printf("51. Foster cultural understanding\n");
+   
+    // 52. Build trust
+    printf("52. Build trust\n");
+   
+    // 53. Reduce dependency on foreign aid
+    printf("53. Reduce dependency on foreign aid\n");
+   
+    // 54. Promote sustainable agriculture
+    printf("54. Promote sustainable agriculture\n");
+   
+    // 55. Encourage trade
+    printf("55. Encourage trade\n");
+   
+    // 56. Clean up conflict zones
+    printf("56. Clean up conflict zones\n");
+   
+    // 57. End sanctions
+    printf("57. End sanctions\n");
+   
+    // 58. Sign peace agreements
+    printf("58. Sign peace agreements\n");
+   
+    // 59. Abolish nuclear weapons
+    printf("59. Abolish nuclear weapons\n");
+   
+    // 60. Strengthen regional organizations
+    printf("60. Strengthen regional organizations\n");
+   
+    // 61. Engage in conflict prevention
+    printf("61. Engage in conflict prevention\n");
+   
+    // 62. Facilitate diplomatic negotiations
+    printf("62. Facilitate diplomatic negotiations\n");
+   
+    // 63. Encourage reconciliation
+    printf("63. Encourage reconciliation\n");
+   
+    // 64. Develop international law
+    printf("64. Develop international law\n");
+   
+    // 65. Develop an international criminal justice system
+    printf("65. Develop an international criminal justice system\n");
+   
+    // 66. Provide developmental assistance
+    printf("66. Provide developmental assistance\n");
+   
+    // 67. Promote accountability
+    printf("67. Promote accountability\n");
+   
+    // 68. Foster mutual understanding
+    printf("68. Foster mutual understanding\n");
+   
+    // 69. Exchange ambassadors
+    printf("69. Exchange ambassadors\n");
+   
+    // 70. Promote regional security
+    printf("70. Promote regional security\n");
+   
+    // 71. Strengthen regional agreements
+    printf("71. Strengthen regional agreements\n");
+   
+    // 72. Respect self-determination
+    printf("72. Respect self-determination\n");
+   
+    // 73. End violence
+    printf("73. End violence\n");
+   
+    // 74. Respect international law
+    printf("74. Respect international law\n");
+   
+    // 75. Eliminate discrimination
+    printf("75. Eliminate discrimination\n");
+   
+    // 76. Promote international environmental law
+    printf("76. Promote international environmental law\n");
+   
+    // 77. Prevent the misuse of power
+    printf("77. Prevent the misuse of power\n");
+   
+    // 78. Foster cooperation
+    printf("78. Foster cooperation\n");
+   
+    // 79. Support the spread of democracy
+    printf("79. Support the spread of democracy\n");
+   
+    // 80. Stop civil wars
+    printf("80. Stop civil wars\n");
+   
+    // 81. Promote global justice
+    printf("81. Promote global justice\n");
+   
+    // 82. Reject autocracy
+    printf("82. Reject autocracy\n");
+   
+    // 83. Promote good governance
+    printf("83. Promote good governance\n");
+   
+    // 84. Control arms proliferation
+    printf("84. Control arms proliferation\n");
+   
+    // 85. Reduce human miseries
+    printf("85. Reduce human miseries\n");
+   
+    // 86. Increase transparency
+    printf("86. Increase transparency\n");
+   
+    // 87. Fight inequality
+    printf("87. Fight inequality\n");
+   
+    // 88. Promote human rights
+    printf("88. Promote human rights\n");
+   
+    // 89. Establish international tribunals
+    printf("89. Establish international tribunals\n");
+   
+    // 90. Improve access to food
+    printf("90. Improve access to food\n");
+   
+    // 91. Invest in public health
+    printf("91. Invest in public health\n");
+   
+    // 92. Support refugees and victims of war
+    printf("92. Support refugees and victims of war\n");
+   
+    // 93. End exploitation of natural resources
+    printf("93. End exploitation of natural resources\n");
+   
+    // 94. Curb the spread of infectious diseases
+    printf("94. Curb the spread of infectious diseases\n");
+   
+    // 95. Protect endangered species
+    printf("95. Protect endangered species\n");
+   
+    // 96. Strengthen international organizations
+    printf("96. Strengthen international organizations\n");
+   
+    // 97. Help conflict-ridden states
+    printf("97. Help conflict-ridden states\n");
+   
+    // 98. Promote respect for the rule of law
+    printf("98. Promote respect for the rule of law\n");
+   
+    // 99. Increase aid flows
+    printf("99. Increase aid flows\n");
+   
+    // 100. Fight poverty
+    printf("100. Fight poverty\n");
+
+    return 0;
+}
