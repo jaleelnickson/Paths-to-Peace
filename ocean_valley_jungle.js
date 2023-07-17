8cf31edc4816884955a9379982327290e5da6951
jaleelnickson <136690980+jaleelnickson@users.noreply.github.com>
2023-07-17 07:59:44 +0700

    Update reporting mechanisms

---
+++ ocean_valley_jungle.js
@@ -0,0 +1,101 @@
+//Paths to Peace in JavaScript
+
+//1. Begin with respect
+
+let respect = "Have respect for yourself and for all people";
+console.log(respect);
+
+//2. Foster Compassion
+
+let compassion = "Foster understanding and acceptance of yourself and others";
+console.log(compassion);
+
+//3. Apply forgiveness
+
+let forgiveness = "Let mistakes, and hurts, be forgiven and forgotten";
+console.log(forgiveness);
+
+//4.Practice Patience
+
+let patience = "Take things at your own pace and don't be too hard on yourself";
+console.log(patience);
+
+//5. Work for Justice
+
+let justice = "Stand up for what is right and for those who are in need";
+console.log(justice);
+
+//6. Pursue Equality
+
+let equality = "Value and embrace diversity and strive for equality among all";
+console.log(equality);
+
+//7. Respect Freedom
+
+let freedom = "Respect the freedoms of others and your own";
+console.log(freedom);
+
+//8.Persevere
+
+let persevere = "Do not give up when goals become difficult to reach";
+console.log(persevere);
+
+//9. Unite
+
+let unite = "Find common ground among different views and realities";
+console.log(unite);
+
+//10. Listen
+
+let listen = "Listen to the voices of those who are unheard";
+console.log(listen);
+
+//11. Uphold Integrity
+
+let integrity = "Be honest and truthful in all of your actions and words";
+console.log(integrity);
+
+//12. Make Amends
+
+let amends = "If necessary, make amends for wrongs or harms done";
+console.log(amends);
+
+//13. Support Others
+
+let support = "Help those in need by offering emotional or practical support";
+console.log(support);
+
+//14. Build Understanding
+
+let understanding = "Be open-minded and seek to understand different backgrounds";
+console.log(understanding);
+
+//15. Practice Gratitude
+
+let gratitude = "Be thankful for everything that makes you who you are";
+console.log(gratitude);
+
+//16. Find Joy
+
+let joy = "Look for the good in every situation and find joy in the moment";
+console.log(joy);
+
+//17. Open your heart
+
+let heart = "Allow yourself to be vulnerable, and open your heart to others";
+console.log(heart);
+
+//18. Make Connections
+
+let connections = "Create meaningful relationships that go beyond surface level";
+console.log(connections);
+
+//19. Pursue Wisdom
+
+let wisdom = "Be curious and strive for knowledge and understanding";
+console.log(wisdom);
+
+//20. Promote Peace
+
+let peace = "Spread peace, happiness, and positivity to everything you do";
+console.log(peace);
