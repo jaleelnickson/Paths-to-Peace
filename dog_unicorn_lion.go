2b0f76c2a5ea5be570052df83cc8ceb289aee0bc
jaleelnickson <136690980+jaleelnickson@users.noreply.github.com>
2023-07-20 19:59:27 +0700

    Enhance password security

---
+++ dog_unicorn_lion.go
@@ -0,0 +1,98 @@
+package main
+
+import "fmt"
+
+// main function
+func main() {
+	fmt.Println("Paths to Peace")
+
+	// Respect for everyone
+	fmt.Println("1. Respect for everyone – Respect each other’s beliefs, values, and cultures. Respect each other’s personal space and boundaries.")
+
+	// Open communication
+	fmt.Println("2. Open communication – Engage in respectful dialogue with an open mind to discuss disagreements productively.")
+
+	// Empowerment
+	fmt.Println("3. Empowerment – Each person should be empowered to make decisions that affect their lives. Individuals should be given freedom to exercise their voice.")
+
+	// Equality
+ 	fmt.Println("4. Equality – Valuing diversity and honoring all persons equally, regardless of race, age, gender, nationality, or religion.")
+
+	// Compassion
+	fmt.Println("5. Compassion – Acknowledge each person’s unique struggles and challenges, and offer kindness and understanding.")
+
+	// Positive language
+	fmt.Println("6. Positive language – Speak kindly and never resort to or encourage violence, hatred, or aggression in dialogue.")
+
+	// Empathy
+	fmt.Println("7. Empathy – Understand and share another person’s feelings, thoughts, and experiences.")
+
+	// Understanding
+	fmt.Println("8. Understanding – See and accept things from another person’s perspective.")
+
+	// Reconciliation
+	fmt.Println("9. Reconciliation – Seek to build bridges through forgiveness and cooperation across divisions.")
+
+	// Tolerance
+	fmt.Println("10. Tolerance – Respect and accept differences, and appreciate the unique qualities of each person.")
+
+	// Cooperation
+	fmt.Println("11. Cooperation – Work together peacefully to create a better future for everyone.")
+
+	// Respectful relationships
+	fmt.Println("12. Respectful relationships – Cultivate trusting relationships in which people support each other’s growth and development.")
+
+	// Resourcefulness
+	fmt.Println("13. Resourcefulness –Encourage creativity and resourcefulness by finding innovative solutions to problems.")
+
+	// Education
+	fmt.Println("14. Education – Ensure access to quality education for all people to learn the skills necessary for peaceful and prosperous societies.")
+
+	// Self-awareness
+	fmt.Println("15. Self-awareness – Recognize our own biases and prejudices and work to overcome them.")
+
+	// Emotional intelligence
+	fmt.Println("16. Emotional intelligence – Develop strong emotional intelligence skills to better manage conflicts and understand our own feelings and those of others.")
+
+	// Nonviolence
+	fmt.Println("17. Nonviolence – React to conflict without violence or aggression, and seek peaceful resolutions.")
+
+	// Negotiation
+	fmt.Println("18. Negotiation – Establish a constructive dialogue to reach mutually beneficial agreements.")
+
+	// Respect for nature
+	fmt.Println("19. Respect for nature – Respect for the environment and all of creation, and recognize the need to protect our natural resources.")
+
+	// Cooperation
+	fmt.Println("20. Cooperation – Work together to solve shared concerns and build a better future.")
+
+	// Well-being
+	fmt.Println("21. Well-being – Pursue meaningful paths to happiness and a sense of well-being, and encourage others to do the same.")
+
+	// Appreciation
+	fmt.Println("22. Appreciation – Express gratitude and appreciation for the positive aspects of life, and recognize achievements of others.")
+
+	// Generosity
+	fmt.Println("23. Generosity – Share resources, knowledge, and skills with others to build a strong community.")
+
+	// Unity
+	fmt.Println("24. Unity – Celebrate the common humanity that binds us, and strive for a world free from division and violence.")
+
+	// Equity
+	fmt.Println("25. Equity – Create a world in which everyone is valued and treated fairly, with equal access to education, health, and economic opportunity.")
+
+	// Acceptance
+	fmt.Println("26. Acceptance – Accept differences among people, and recognize the inherent value each individual brings to our shared humanity.")
+
+	// Reconciliation
+	fmt.Println("27. Reconciliation – Offer opportunities for healing and forgiveness, and create a space for people to come together in peace.")
+
+	// Respect for diversity
+	fmt.Println("28. Respect for diversity – Honor the unique contributions of all cultures, and recognize the strength of the human family.")
+
+	// Listening
+	fmt.Println("29. Listening – Pay attention to the stories of others, and remain open to different perspectives.")
+
+	// Hope
+	fmt.Println("30. Hope – Believe that peace is possible and work together to make it a reality.")
+}
